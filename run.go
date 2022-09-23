cb2fd1b60d587b06899f199ec88d4ed317ff8871
Robert Rees <shudderfix@gmail.com>
2022-09-23 16:03:32 +0100

    Experiments with some basic argument handling

--- run.go
+++ run.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(D(RollRequest{2, 3, -3}))
+	arguments := os.Args[1:]
+
+	numberOfDice := 2
+
+	if(len(arguments) > 0) {
+		convertedNumber, err := strconv.Atoi(arguments[0])
+		if err == nil {
+			numberOfDice = convertedNumber
+		}
+	}
+
+	rolls := make([]RollResult, numberOfDice)
+
+	for i := 0; i < numberOfDice; i++ {
+		rolls[i] = D(RollRequest{2, 3, -3})
+	}
+
+	fmt.Println(rolls)
 }
